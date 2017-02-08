84a65606b754e02a9a0136f8f94b2f2530a6056a
Andrew Devlin and Matt Kelley <andrew@>
2017-02-08 15:49:18 -0800

    add gifs to page and made dead panel opaque

--- app/app.php
+++ app/app.php
@@ -22,7 +22,22 @@
             if ($_SESSION['tama'][0]->getPlay() <= 0 || $_SESSION['tama'][0]->getSleep() <= 0 || $_SESSION['tama'][0]->getFood() <= 0) {
                 return $app['twig']->render("Dead.html.twig");
             } else {
-                return $app['twig']->render('homeView.html.twig', array('tama' => Tama::getAll()));
+                $play = $_SESSION['tama'][0]->getPlay();
+                $sleep = $_SESSION['tama'][0]->getSleep();
+                $food = $_SESSION['tama'][0]->getFood();
+                $level = (($play + $sleep + $food)/300)*100;
+                if($level>=90){
+                  $health="https://media.giphy.com/media/NDO26aNYH0bXq/giphy.gif";
+                }else if ($level>=70){
+                  $health="https://media.giphy.com/media/12Bpme5pTzGmg8/giphy.gif";
+                }else if($level>=60){
+                  $health="https://media.giphy.com/media/ghUeyOoEx3uPS/giphy.gif";
+                }else if($level>=40){
+                  $health = "http://static.fjcdn.com/gifs/Lucario+bein+a+thug+pokemon+for+kids+they+say_7b1c90_5183486.gif";
+                }else{
+                  $health = "http://data.whicdn.com/images/34312692/large.gif";
+                }
+                return $app['twig']->render('homeView.html.twig', array('tama' => Tama::getAll(), 'health'=>$health));
             }
         }else {
           return $app['twig']->render('homeView.html.twig', array('tama' => Tama::getAll()));
