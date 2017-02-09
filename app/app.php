95cbc39add9f292fbe80e3ec1062e5fd96fd786b
Andrew Devlin and Matt Kelley <andrew@>
2017-02-08 16:20:31 -0800

    improved health function

--- app/app.php
+++ app/app.php
@@ -26,16 +26,16 @@
                 $sleep = $_SESSION['tama'][0]->getSleep();
                 $food = $_SESSION['tama'][0]->getFood();
                 $level = (($play + $sleep + $food)/300)*100;
-                if($level>=90){
-                  $health="https://media.giphy.com/media/NDO26aNYH0bXq/giphy.gif";
-                }else if ($level>=70){
-                  $health="https://media.giphy.com/media/12Bpme5pTzGmg8/giphy.gif";
-                }else if($level>=60){
-                  $health="https://media.giphy.com/media/ghUeyOoEx3uPS/giphy.gif";
-                }else if($level>=40){
-                  $health = "http://static.fjcdn.com/gifs/Lucario+bein+a+thug+pokemon+for+kids+they+say_7b1c90_5183486.gif";
-                }else{
+                if($level<=20 || $play <= 20 || $sleep <= 20 || $food <= 20){
                   $health = "http://data.whicdn.com/images/34312692/large.gif";
+                }else if ($level<=40 || $play <= 40 || $sleep <= 40 || $food <= 40){
+                  $health = "http://static.fjcdn.com/gifs/Lucario+bein+a+thug+pokemon+for+kids+they+say_7b1c90_5183486.gif";
+                }else if($level<=60 || $play <= 60 || $sleep <= 60 || $food <= 60){
+                  $health="https://media.giphy.com/media/ghUeyOoEx3uPS/giphy.gif";
+                }else if($level<=80 || $play <= 80 || $sleep <= 80 || $food <= 80){
+                  $health="https://media.giphy.com/media/12Bpme5pTzGmg8/giphy.gif";
+                }else{
+                  $health="https://media.giphy.com/media/NDO26aNYH0bXq/giphy.gif";
                 }
                 return $app['twig']->render('homeView.html.twig', array('tama' => Tama::getAll(), 'health'=>$health));
             }
